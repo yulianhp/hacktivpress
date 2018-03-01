3c111a61a5969cc6457eb5773c170348aaf4ce11
yulianhp <kono.utsukushi24@gmail.com>
2018-03-01 14:18:02 +0700

    install dotenv, signin&signup done, added endpoint to user router

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -35,7 +35,40 @@ class User {
     user.findOne({
       username: input.username
     }).then(result => {
-      console.log(result);
+      // console.log(result);
+      if (bcrypt.compareSync(input.password, result.password) == false) {
+        res.status(400)
+        .json({
+          msg: 'email or password did not match'
+        })
+      }
+      let payload = {
+        uid: result._id,
+        username: result.username
+      }
+      // console.log(payload);
+      jwt.sign(payload, process.env.SECRET_KEY, (err, token) => {
+        if(!err){
+          console.log(token);
+          res.status(200)
+          .json({
+            msg: 'Logged in',
+            token: token
+          })
+        } else {
+          res.status(500)
+          .json({
+            msg: 'Failed',
+            error: err
+          })
+        }
+      })
+    }).catch(err => {
+      res.status(400)
+      .json({
+        msg: 'User not found',
+        error: err
+      })
     })
   }
 }
