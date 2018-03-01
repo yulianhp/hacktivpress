c2f0951211616e12ad8bf0480645eb2148e19bb4
yulianhp <kono.utsukushi24@gmail.com>
2018-03-01 14:53:32 +0700

    article controller & router

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -46,7 +46,7 @@ class Article {
   }
   
   static getByUser(req,res){
-    article.find()
+    article.find({author: req.params.id})
     .then(result => {
       res.status(200)
       .json({
@@ -62,6 +62,105 @@ class Article {
     })
   }
   
+  static getCatNews(req,res){
+    article.find({category: 'news'})
+    .then(result => {
+      res.status(200)
+      .json({
+        msg: 'List of articles in news category',
+        articles: result
+      })
+    }).catch(err => {
+      res.status(500)
+      .json({
+        msg: 'Cant get articles from news category',
+        error: err
+      })
+    })
+  }
+
+  static getCatReviews(req,res){
+    article.find({category: 'review'})
+    .then(result => {
+      res.status(200)
+      .json({
+        msg: 'List of articles in reviews category',
+        articles: result
+      })
+    }).catch(err => {
+      res.status(500)
+      .json({
+        msg: 'Cant get articles from reviews category',
+        error: err
+      })
+    })
+  }
+  
+  static getOne(req,res){
+    article.findOne({'_id': req.params.id})
+    .then(result => {
+      res.status(200)
+      .json({
+        msg: 'Show detail',
+        article: result
+      })
+    }).catch(err => {
+      res.status(500)
+      .json({
+        msg: 'Cant get detail of article',
+        error: err
+      })
+    })
+  }
+  
+  static editOne(req,res){
+    let input = {
+      title: req.body.title,
+      content: req.body.content,
+      category: req.body.category
+    }
+    article.findOneAndUpdate({
+      "_id": req.params.id
+    },{
+      $set:{
+        "title": input.title,
+        "content": input.content,
+        "category": input.category
+      }
+    },{
+      new: true
+    }).then(result => {
+      res.status(200)
+      .json({
+        msg: 'Article updated',
+        article: result
+      })
+    }).catch(err => {
+      res.status(500)
+      .json({
+        msg: 'Cant update article',
+        error: err
+      })
+    })
+  }
+  
+  static deleteOne(req,res){
+    article.remove({"_id": req.params.id})
+    .then(result => {
+      res.status(200)
+      .json({
+        msg: 'Deleted',
+        result: result
+      })
+    }).catch(err => {
+      res.status(500)
+      .json({
+        msg: 'Cant delete article',
+        error: err
+      })
+    })
+  }
+    
 }
 
 module.exports = Article;
--- server/routes/articles.js
+++ server/routes/articles.js
@@ -4,6 +4,11 @@ const Article = require('../controllers/article');
 
 router.post('/', Article.createOne)
 router.get('/', Article.getAll)
-
+router.get('/user/:id', Article.getByUser)
+router.get('/category/news', Article.getCatNews)
+router.get('/category/reviews', Article.getCatReviews)
+router.get('/detail/:id', Article.getOne)
+router.put('/edit/:id', Article.editOne)
+router.delete('/delete/:id', Article.deleteOne)
 
 module.exports = router;
