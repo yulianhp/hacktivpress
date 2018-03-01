2e53f599d695b4ad92798aeebab95d8fa957cf4c
yulianhp <kono.utsukushi24@gmail.com>
2018-03-01 15:10:20 +0700

    istirahat 15 menit

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import LoginPage from '@/views/LoginPage'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'LoginPage',
+      component: LoginPage
     }
   ]
 })
---
+++ client/src/store/store.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+const http = axios.create({
+  baseURL: 'http://localhost:3000/api/'
+})
+
+export const store = new Vuex.Store({
+  state: { }
+})
